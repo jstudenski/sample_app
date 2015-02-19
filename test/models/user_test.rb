7a8a6b4076c8ab72345807c22a53cb9383e7bc69
jstudenski <jstudenski@gmail.com>
2015-02-18 19:06:28 -0600

    removed extra 'end'

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,7 +59,6 @@ class UserTest < ActiveSupport::TestCase
 		@user.password = @user.password_confirmation = "a" * 5
 		assert_not @user.valid?
 	end
-end
 
   # test "the truth" do
   #   assert true
